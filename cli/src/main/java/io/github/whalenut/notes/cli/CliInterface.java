ecb2e3eb71a4ce6076ae2fab82e1bf736d319579
Daniel Figueroa <daniel.figueroa@doktor24.se>
2020-11-17 00:17:30 +0100

    Commands are working but are intialized by jline instead of dagger.

--- cli/src/main/java/io/github/whalenut/notes/cli/CliInterface.java
+++ cli/src/main/java/io/github/whalenut/notes/cli/CliInterface.java
@@ -1,13 +1,16 @@
 package io.github.whalenut.notes.cli;
 
 import io.github.whalenut.notes.core.NoteRepository;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.ParentCommand;
 
 import javax.inject.Inject;
 
-@Command(name = "test",
+@Command(name = "",
         description = "A test",
-        footer = {"", "Press Ctrl-D to exit."})
+        footer = {"", "Press Ctrl-D to exit."},
+        subcommands = {CliInterface.FooCommand.class})
 public class CliInterface implements Runnable {
 
     private final NoteRepository noteRepository;
@@ -23,4 +26,21 @@ public class CliInterface implements Runnable {
     public void run() {
         System.out.println("Command is run.");
     }
+
+    @Command(name = "test",
+            description = "A simple test")
+    public static class FooCommand implements Runnable {
+
+        @ParentCommand CliInterface parent;
+
+        public FooCommand() {
+            System.out.println("Creating the FooCommand.");
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Hello World!");
+        }
+    }
+
 }
